9df59da8a8e66973fd9f965d9fff53d3d57a4c18
Tony <tony@gemail.com>
2019-07-01 08:30:55 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.example.coolweather.gson;
+
+public class AQI {
+
+    public AOICity city;
+
+    public class AOICity{
+
+        public String aqi;
+        public String pm25;
+    }
+}
